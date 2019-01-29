075b67c69df505b1b3dc94059131b4464449ff11
KARgantua <smlee@info-works.co.kr>
2019-01-29 11:16:00 +0900

    Write, Read 코드 작성 완료

--- APDS9960/APDS9960_I2C/APDS9960_I2C.ino
+++ APDS9960/APDS9960_I2C/APDS9960_I2C.ino
@@ -9,6 +9,10 @@
 #define GESTURE_SENSITIVITY_1   50
 #define GESTURE_SENSITIVITY_2   20
 
+//장치 ID
+#define APDS9960_ID_1           0xAB
+#define APDS9960_ID_2           0x9C 
+
 //에러코드
 #define ERROR                   0xFF
 
@@ -73,8 +77,133 @@
 #define APDS9960_GEN            0b01000000
 #define APDS9960_GVALID         0b00000001
 
-void setup() {
+//On/Off 정의
+#define OFF                     0
+#define ON                      1
 
+//setMode에 대한 인자
+#define POWER                   0
+#define AMBIENT_LIGHT           1
+#define PROXIMITY               2
+#define WAIT                    3
+#define AMBIENT_LIGHT_INT       4
+#define PROXIMITY_INT           5
+#define GESTURE                 6
+#define ALL                     7
+
+//LED Drive 값
+#define LED_DRIVE_100MA         0
+#define LED_DRIVE_50MA          1
+#define LED_DRIVE_25MA          2
+#define LED_DRIVE_12_5MA        3
+
+//근접 이득 값
+#define PGAIN_1X                0
+#define PGAIN_2X                1
+#define PGAIN_4X                2
+#define PGAIN_8X                3
+
+//ALS 이득 값
+#define AGAIN_1X                0
+#define AGAIN_4X                1
+#define AGAIN_16X               2
+#define AGAIN_64X               3
+
+//제스쳐 이득 값
+#define GGAIN_1X                0
+#define GGAIN_2X                1
+#define GGAIN_4X                2
+#define GGAIN_8X                3
+
+//LED 부스트 값
+#define LED_BOOST_100           0
+#define LED_BOOST_150           1
+#define LED_BOOST_200           2
+#define LED_BOOST_300           3    
+
+//제스쳐 기다리는 시간
+#define GWTIME_0MS              0
+#define GWTIME_2_8MS            1
+#define GWTIME_5_6MS            2
+#define GWTIME_8_4MS            3
+#define GWTIME_14_0MS           4
+#define GWTIME_22_4MS           5
+#define GWTIME_30_8MS           6
+#define GWTIME_39_2MS           7
+
+//기본값
+#define DEFAULT_ATIME           219     // 103ms
+#define DEFAULT_WTIME           246     // 27ms
+#define DEFAULT_PROX_PPULSE     0x87    // 16us, 8 pulses
+#define DEFAULT_GESTURE_PPULSE  0x89    // 16us, 10 pulses
+#define DEFAULT_POFFSET_UR      0       // 0 offset
+#define DEFAULT_POFFSET_DL      0       // 0 offset      
+#define DEFAULT_CONFIG1         0x60    // No 12x wait (WTIME) factor
+#define DEFAULT_LDRIVE          LED_DRIVE_100MA
+#define DEFAULT_PGAIN           PGAIN_4X
+#define DEFAULT_AGAIN           AGAIN_4X
+#define DEFAULT_PILT            0       // Low proximity threshold
+#define DEFAULT_PIHT            50      // High proximity threshold
+#define DEFAULT_AILT            0xFFFF  // Force interrupt for calibration
+#define DEFAULT_AIHT            0
+#define DEFAULT_PERS            0x11    // 2 consecutive prox or ALS for int.
+#define DEFAULT_CONFIG2         0x01    // No saturation interrupts or LED boost  
+#define DEFAULT_CONFIG3         0       // Enable all photodiodes, no SAI
+#define DEFAULT_GPENTH          40      // Threshold for entering gesture mode
+#define DEFAULT_GEXTH           30      // Threshold for exiting gesture mode    
+#define DEFAULT_GCONF1          0x40    // 4 gesture events for int., 1 for exit
+#define DEFAULT_GGAIN           GGAIN_4X
+#define DEFAULT_GLDRIVE         LED_DRIVE_100MA
+#define DEFAULT_GWTIME          GWTIME_2_8MS
+#define DEFAULT_GOFFSET         0       // No offset scaling for gesture mode
+#define DEFAULT_GPULSE          0xC9    // 32us, 10 pulses
+#define DEFAULT_GCONF3          0       // All photodiodes active during gesture
+#define DEFAULT_GIEN            0       // Disable gesture interrupts
+
+int APDS9960_Write(uint8_t reg, uint8_t *val, uint8_t size) {
+    Wire.beginTransmission(APDS9960_I2C_ADDRESS);
+    Wire.write(reg);
+    Wire.write(val, size);
+    if (Wire.endTransmission() != 0)
+        return 0;
+    return 1;
+}
+
+int APDS9960_Write_reg(uint8_t reg, uint8_t *val) {
+    int error;
+    error = APDS9960_Write(reg, val, 1);
+    return error;
+}
+
+/*  I2C  Combined 포맷 형식
+    SA -> RA -> SA -> Data
+*/
+int APDS9960_Read(uint8_t reg, uint8_t *buf, int size) {
+    int i;
+    Wire.beginTransmission(APDS9960_I2C_ADDRESS);
+    Wire.write(reg);
+    Wire.requestFrom(APDS9960_I2C_ADDRESS, size);
+    while(Wire.available() && i < size){
+        buf[i++] = Wire.read();
+    }
+    if (size != i) 
+        return 0;
+    
+    return 1;
+}
+
+void APDS9960_Reset() {
+    
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Serial Connected");
+
+    Wire.begin();
+    Serial.println("I2C Connected");
+
+    APDS9960_Init();
 }
 
 void loop() {
