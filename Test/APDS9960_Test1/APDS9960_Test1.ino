4e9a05a248ade464f5b26273ffd8a7e67898fa97
KARgantua <smlee@info-works.co.kr>
2019-01-29 17:54:51 +0900

    오류 init에서 문제 id 레지스터 안받아와짐

--- Test/APDS9960_Test1/APDS9960_Test1.ino
+++ Test/APDS9960_Test1/APDS9960_Test1.ino
@@ -66,21 +66,217 @@
 #define APDS9960_GFIFO_L        0xFE
 #define APDS9960_GFIFO_R        0xFF
 
-void APDS9960_Write(uint8_t reg, uint8_t cmd) {
+//setMode에 대한 인자
+#define POWER                   0
+#define AMBIENT_LIGHT           1
+#define PROXIMITY               2
+#define WAIT                    3
+#define AMBIENT_LIGHT_INT       4
+#define PROXIMITY_INT           5
+#define GESTURE                 6
+#define ALL                     7
+
+
+//LED Drive 값
+#define LED_DRIVE_100MA         0
+#define LED_DRIVE_50MA          1
+#define LED_DRIVE_25MA          2
+#define LED_DRIVE_12_5MA        3
+
+//근접 이득 값
+#define PGAIN_1X                0
+#define PGAIN_2X                1
+#define PGAIN_4X                2
+#define PGAIN_8X                3
+
+//ALS 이득 값
+#define AGAIN_1X                0
+#define AGAIN_4X                1
+#define AGAIN_16X               2
+#define AGAIN_64X               3
+
+//제스쳐 이득 값
+#define GGAIN_1X                0
+#define GGAIN_2X                1
+#define GGAIN_4X                2
+#define GGAIN_8X                3
+
+//LED 부스트 값
+#define LED_BOOST_100           0
+#define LED_BOOST_150           1
+#define LED_BOOST_200           2
+#define LED_BOOST_300           3    
+
+//제스쳐 기다리는 시간
+#define GWTIME_0MS              0
+#define GWTIME_2_8MS            1
+#define GWTIME_5_6MS            2
+#define GWTIME_8_4MS            3
+#define GWTIME_14_0MS           4
+#define GWTIME_22_4MS           5
+#define GWTIME_30_8MS           6
+#define GWTIME_39_2MS           7
+
+//기본값
+#define DEFAULT_ATIME           219     // 103ms
+#define DEFAULT_WTIME           246     // 27ms
+#define DEFAULT_PROX_PPULSE     0x87    // 16us, 8 pulses
+#define DEFAULT_GESTURE_PPULSE  0x89    // 16us, 10 pulses
+#define DEFAULT_POFFSET_UR      0       // 0 offset
+#define DEFAULT_POFFSET_DL      0       // 0 offset      
+#define DEFAULT_CONFIG1         0x60    // No 12x wait (WTIME) factor
+#define DEFAULT_LDRIVE          LED_DRIVE_100MA
+#define DEFAULT_PGAIN           PGAIN_4X
+#define DEFAULT_AGAIN           AGAIN_4X
+#define DEFAULT_PILT            0       // Low proximity threshold
+#define DEFAULT_PIHT            50      // High proximity threshold
+#define DEFAULT_AILT            0xFFFF  // Force interrupt for calibration
+#define DEFAULT_AIHT            0
+#define DEFAULT_PERS            0x11    // 2 consecutive prox or ALS for int.
+#define DEFAULT_CONFIG2         0x01    // No saturation interrupts or LED boost  
+#define DEFAULT_CONFIG3         0       // Enable all photodiodes, no SAI
+#define DEFAULT_GPENTH          40      // Threshold for entering gesture mode
+#define DEFAULT_GEXTH           30      // Threshold for exiting gesture mode    
+#define DEFAULT_GCONF1          0x40    // 4 gesture events for int., 1 for exit
+#define DEFAULT_GGAIN           GGAIN_4X
+#define DEFAULT_GLDRIVE         LED_DRIVE_100MA
+#define DEFAULT_GWTIME          GWTIME_2_8MS
+#define DEFAULT_GOFFSET         0       // No offset scaling for gesture mode
+#define DEFAULT_GPULSE          0xC9    // 32us, 10 pulses
+#define DEFAULT_GCONF3          0       // All photodiodes active during gesture
+#define DEFAULT_GIEN            0       // Disable gesture interrupts
+
+int APDS9960_Write(uint8_t reg, uint8_t cmd) {
     Wire.beginTransmission(APDS9960_I2C_ADDRESS);
     Wire.write(reg);
     Wire.write(cmd);
     Wire.endTransmission();
+
+    return 1;
 }
 
-void APDS9960_Read(uint8_t reg, uint8_t *buf, int size) {
-    int i;
+int APDS9960_Read(uint8_t reg, uint8_t *buf, int size) {
+    int i, n;
     Wire.beginTransmission(APDS9960_I2C_ADDRESS);
-    Wire.write(reg);
+    n = Wire.write(reg);
+    if(n != 1) {
+        Serial.println("Fail write");
+        return 0;
+    }
     Wire.requestFrom(APDS9960_I2C_ADDRESS, size);
     while(Wire.available() && i < size) {
         buf[i++] = Wire.read();
     }
+    Serial.println(*buf);
+    return 1;
+}
+
+int APDS9960_setAmbientLightIntEnable(uint8_t enable)
+{
+    uint8_t val;
+    
+    if( !APDS9960_Read(APDS9960_ENABLE, val, 1) ) {
+        return 0;
+    }
+    
+    enable &= 0b00000001;
+    enable = enable << 4;
+    val &= 0b11101111;
+    val |= enable;
+    
+    if( !APDS9960_Write(APDS9960_ENABLE, val) ) {
+        return 0;
+    }
+    
+    return 1;
+}
+
+int APDS9960_setAmbientLightGain(uint8_t drive) {
+    uint8_t val;
+
+    APDS9960_Read(APDS9960_CONTROL, val, 1);
+
+    drive &= 0b00000011;
+    val &= 0b11111100;
+    val |= drive;
+
+    APDS9960_Write(APDS9960_CONTROL, val);
+
+    return 1;
+}
+
+uint8_t APDS9960_getMode()
+{
+    uint8_t enable_value;
+    
+    if( !APDS9960_Read(APDS9960_ENABLE, enable_value, 1) ) {
+        return ERROR;
+    }
+    
+    return enable_value;
+}
+
+int APDS9960_setMode(uint8_t mode, uint8_t enable)
+{
+    uint8_t reg_val;
+
+    reg_val = APDS9960_getMode();
+    if( reg_val == ERROR ) {
+        return 0;
+    }
+    
+    enable = enable & 0x01;
+    if( mode >= 0 && mode <= 6 ) {
+        if (enable) {
+            reg_val |= (1 << mode);
+        } else {
+            reg_val &= ~(1 << mode);
+        }
+    } else if( mode == ALL ) {
+        if (enable) {
+            reg_val = 0x7F;
+        } else {
+            reg_val = 0x00;
+        }
+    }
+        
+    if( !APDS9960_Write(APDS9960_ENABLE, reg_val) ) {
+        return 0;
+    }
+        
+    return 1;
+}
+
+int APDS9960_enablePower()
+{
+    if( !APDS9960_setMode(POWER, 1) ) {
+        return 0;
+    }
+    
+    return 1;
+}
+
+int APDS9960_enableLightSensor(int interrupt) {
+    if (!APDS9960_setAmbientLightGain(DEFAULT_AGAIN)) {
+        return 0;
+    }
+    if (interrupt) {
+        if(!APDS9960_setAmbientLightIntEnable(1)) {
+            return 0;
+        }
+    } else {
+        if(!APDS9960_setAmbientLightIntEnable(0)) {
+            return 0;
+        }
+    }
+    if(!APDS9960_enablePower()) {
+        return 0;
+    }
+    if(!APDS9960_setMode(AMBIENT_LIGHT, 1)) {
+        return 0;
+    }
+
+    return 1;
 }
 
 int APDS9960_Init() {
@@ -88,12 +284,33 @@ int APDS9960_Init() {
     //ID 확인
     uint8_t id;
     APDS9960_Read(APDS9960_ID, &id, 1);
+    Serial.println(id);
     if(id != 0xAB) {
         return 0;
     }
-
     //ENABLE 레지스터 전부 켜기
     APDS9960_Write(APDS9960_ENABLE, 0x7F);      //MSB는 Reserved 비트
+
+    return 1;
+}
+
+int APDS9960_readAmbientLight(uint16_t &val)
+{
+    uint8_t val_byte;
+    val = 0;
+    
+    if( !APDS9960_Read(APDS9960_CDATAL, val_byte, 1) ) {
+        return 0;
+    }
+    val = val_byte;
+    
+    /* Read value from clear channel, high byte register */
+    if( !APDS9960_Read(APDS9960_CDATAH, val_byte, 1) ) {
+        return 0;
+    }
+    val = val + ((uint16_t)val_byte << 8);
+    
+    return 1;
 }
 
 void setup(void) {
@@ -107,9 +324,16 @@ void setup(void) {
         while(1) {}
     }
     //RGB Ambient Light 허용
-    APDS9960_EnableLightSensor(false);
+    APDS9960_enableLightSensor(0);
 }
 
 void loop(void) {
-
+    uint16_t ambientlight;
+    if(!APDS9960_readAmbientLight(ambientlight)) {
+        Serial.println("ERROR");
+    } else {
+        Serial.print("Ambient:\t");
+        Serial.println(ambientlight);
+    }
+    delay(100);
 }
