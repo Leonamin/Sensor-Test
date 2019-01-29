d5a35e96b1fd058fa6d2b218892af0829e7e0758
KARgantua <smlee@info-works.co.kr>
2019-01-29 16:33:01 +0900

    Test 파일 생성, APDS9960 간이 테스트 코드 작성중

---
+++ Test/APDS9960_Test1/APDS9960_Test1.ino
@@ -0,0 +1,115 @@
+#include <Wire.h>
+
+//APDS-9960 I2C Address
+#define APDS9960_I2C_ADDRESS 0x39
+ 
+//제스쳐 인자
+#define GESTURE_THRESHOLD_OUT   10
+#define GESTURE_SENSITIVITY_1   50
+#define GESTURE_SENSITIVITY_2   20
+
+//장치 ID
+#define APDS9960_ID_1           0xAB
+#define APDS9960_ID_2           0x9C 
+
+//에러코드
+#define ERROR                   0xFF
+
+//APDS-9960 레지스터 주소
+#define APDS9960_ENABLE         0x80
+#define APDS9960_ATIME          0x81
+#define APDS9960_WTIME          0x83
+#define APDS9960_AILTL          0x84
+#define APDS9960_AILTH          0x85
+#define APDS9960_AIHTL          0x86
+#define APDS9960_AIHTH          0x87
+#define APDS9960_PILT           0x89
+#define APDS9960_PIHT           0x8B
+#define APDS9960_PERS           0x8C
+#define APDS9960_CONFIG1        0x8D
+#define APDS9960_PPULSE         0x8E
+#define APDS9960_CONTROL        0x8F
+#define APDS9960_CONFIG2        0x90
+#define APDS9960_ID             0x92
+#define APDS9960_STATUS         0x93
+#define APDS9960_CDATAL         0x94
+#define APDS9960_CDATAH         0x95
+#define APDS9960_RDATAL         0x96
+#define APDS9960_RDATAH         0x97
+#define APDS9960_GDATAL         0x98
+#define APDS9960_GDATAH         0x99
+#define APDS9960_BDATAL         0x9A
+#define APDS9960_BDATAH         0x9B
+#define APDS9960_PDATA          0x9C
+#define APDS9960_POFFSET_UR     0x9D
+#define APDS9960_POFFSET_DL     0x9E
+#define APDS9960_CONFIG3        0x9F
+#define APDS9960_GPENTH         0xA0
+#define APDS9960_GEXTH          0xA1
+#define APDS9960_GCONF1         0xA2
+#define APDS9960_GCONF2         0xA3
+#define APDS9960_GOFFSET_U      0xA4
+#define APDS9960_GOFFSET_D      0xA5
+#define APDS9960_GOFFSET_L      0xA7
+#define APDS9960_GOFFSET_R      0xA9
+#define APDS9960_GPULSE         0xA6
+#define APDS9960_GCONF3         0xAA
+#define APDS9960_GCONF4         0xAB
+#define APDS9960_GFLVL          0xAE
+#define APDS9960_GSTATUS        0xAF
+#define APDS9960_IFORCE         0xE4
+#define APDS9960_PICLEAR        0xE5
+#define APDS9960_CICLEAR        0xE6
+#define APDS9960_AICLEAR        0xE7
+#define APDS9960_GFIFO_U        0xFC
+#define APDS9960_GFIFO_D        0xFD
+#define APDS9960_GFIFO_L        0xFE
+#define APDS9960_GFIFO_R        0xFF
+
+void APDS9960_Write(uint8_t reg, uint8_t cmd) {
+    Wire.beginTransmission(APDS9960_I2C_ADDRESS);
+    Wire.write(reg);
+    Wire.write(cmd);
+    Wire.endTransmission();
+}
+
+void APDS9960_Read(uint8_t reg, uint8_t *buf, int size) {
+    int i;
+    Wire.beginTransmission(APDS9960_I2C_ADDRESS);
+    Wire.write(reg);
+    Wire.requestFrom(APDS9960_I2C_ADDRESS, size);
+    while(Wire.available() && i < size) {
+        buf[i++] = Wire.read();
+    }
+}
+
+int APDS9960_Init() {
+
+    //ID 확인
+    uint8_t id;
+    APDS9960_Read(APDS9960_ID, &id, 1);
+    if(id != 0xAB) {
+        return 0;
+    }
+
+    //ENABLE 레지스터 전부 켜기
+    APDS9960_Write(APDS9960_ENABLE, 0x7F);      //MSB는 Reserved 비트
+}
+
+void setup(void) {
+    Serial.begin(115200);
+    Serial.println("Serial Connected");
+    Wire.begin();
+    Serial.println("I2C Connected");
+
+    if(!APDS9960_Init()) {
+        Serial.println("Failed to call Device");
+        while(1) {}
+    }
+    //RGB Ambient Light 허용
+    APDS9960_EnableLightSensor(false);
+}
+
+void loop(void) {
+
+}
